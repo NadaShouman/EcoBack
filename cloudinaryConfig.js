9ac65557bc10a09ac37339deae80572be10ae75e
Habiba Magdy <62236824+habibamagdy30@users.noreply.github.com>
2023-03-02 15:43:13 +0200

    Update cloudinaryConfig.js

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -1,1 +1,15 @@
+const cloudinary = require('cloudinary');
+const { Promise } = require('mongoose');
+cloudinary.config({
+    cloud_name:'dalk6iico',
+    api_key:'354216316393478',
+    api_secret:'2t1XZOb7vimbi0D9hAgb5u8pbpw'
+});
 
+exports.uploads = (file) =>{
+    return new Promise( resolve =>{
+        cloudinary.uploader.upload(file,(result)=>{
+            resolve({url:result.url,id:result.public_id})
+        },{resource_type : "auto"})
+    })
+}
